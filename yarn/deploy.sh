3bd05bddedebb4151e1c63a51f5cfcd6f31f4159
Vlad <vladka@webdevelop.pro>
2024-07-04 21:54:38 +0300

    up

--- yarn/deploy.sh
+++ yarn/deploy.sh
@@ -1,5 +1,5 @@
-docker build -t gcr.io/unfederalreserve-dev/yarn:`date +%d%m%Y` .
-docker push gcr.io/unfederalreserve-dev/yarn:`date +%d%m%Y`
+docker build -t gcr.io/webdevelop-dev/yarn:`date +%d%m%Y` .
+docker push gcr.io/webdevelop-dev/yarn:`date +%d%m%Y`
 
-docker build -t gcr.io/unfederalreserve-dev/yarn:latest .
-docker push gcr.io/unfederalreserve-dev/yarn:latest
+docker build -t gcr.io/webdevelop-dev/yarn:latest .
+docker push gcr.io/webdevelop-dev/yarn:latest
